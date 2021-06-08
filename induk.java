0c2485fa186c5ae230be47c749bb4ba1976c1146
ZafriMahendraPutra007 <85572027+ZafriMahendraPutra007@users.noreply.github.com>
2021-06-08 23:07:50 +0700

    Add files via upload

---
+++ induk.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package keywoardsuperdanextends;
+
+/**
+ *
+ * @author zafri
+ */
+public class induk {
+    String nama="Zafri Mahendra Putra";
+    int kelas= 10;
+    int umur= 17;
+public static void main(String[] args){
+    //Menjalankan Object
+    subclass word=new subclass();
+    //Menampilkan Method Dari Class Induk
+    word.induk();
+
+}}
---
+++ subclass.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package keywoardsuperdanextends;
+
+/**
+ *
+ * @author zafri
+ */
+public class subclass extends induk{
+   
+    void induk(){
+        
+        System.out.println("Nama: "+super.nama);
+        System.out.println("Umur: "+super.umur);
+        System.out.println("Kelas: "+super.kelas);
+    
+    }}
